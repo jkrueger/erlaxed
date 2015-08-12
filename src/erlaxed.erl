f9fcf8ec9976106575f0df6ad41b8873dde7623e
Jan Krüger <jan.krueger@gmail.com>
2015-08-12 12:42:44 +0200

    Initial code commit

---
+++ src/erlaxed.erl
@@ -0,0 +1,106 @@
+%%
+%%  @author    Jan Krueger <jan.krueger@gmail.com>
+%%  @copyright 2015 Jan Krueger
+%%
+%%% This file is part of erlaxed released under the MIT license.
+%%
+
+-module(erlaxed).
+-export([config/0, config/2]).
+-export([connect/2]).
+-export([delete/1, delete/2]).
+-export([store/2, store/3]).
+-export([view/2, view/3]).
+
+-spec config() -> any().
+config() ->
+    config(<<"http://127.0.0.1:5984">>, []).
+
+-spec config(string() | binary(), map()) -> any().
+config(Host, Options) ->
+    #{ host    => etbx:to_binary(Host),
+       options => Options }.
+
+-spec connect(any(), binary() | string()) -> any().
+connect(Config, Name0) ->
+    Name = etbx:to_binary(Name0),
+    DB   = etbx:update(db, Name, Config),
+    ensure_db(DB).
+
+delete(DB) ->
+    strip_body(erlaxed_client:delete(DB)).
+
+delete(DB, Doc) ->
+    case get_value(<<"_id">>, Doc) of
+        undefined ->
+            {error, not_a_doc};
+        Id ->
+            erlaxed_client:delete(DB, Id)
+    end.
+
+store(DB, Doc) ->
+    store(DB, get_value(<<"_id">>, Doc), Doc).
+
+store(DB, undefined, Doc) ->
+    return_doc(DB, erlaxed_client:post(DB, to_json(Doc)));
+store(DB, Id, Doc) ->
+    erlaxed_client:put(DB, Id, to_json(Doc)).
+
+%% @private
+ensure_db(DB) ->
+    ensure_db(strip_body(erlaxed_client:get(DB)), DB).
+ensure_db(ok, DB) ->
+    {ok, DB};
+ensure_db({error, not_found}, DB) ->
+    maybe(erlaxed_client:put(DB), DB);
+ensure_db(Error, _) ->
+    Error.
+
+view(DB, View) ->
+    view(DB, View, []).
+
+view(DB, {Design0, View0}, Options) ->
+    Design = etbx:to_binary(Design0),
+    View   = etbx:to_binary(View0),
+    Endpoint = <<"_design/", Design/binary, "/_view/", View/binary>>,
+    erlaxed_client:get(DB, Endpoint, Options).
+
+%% @private
+return_doc(DB, {ok, Handle}) ->
+    return_doc(DB, get_value(<<"id">>, Handle));
+return_doc(_, undefined) ->
+    throw({error, api, "POST on a document did not return an id"});
+return_doc(DB, Id) when is_binary(Id) ->
+    erlaxed_client:get(DB, Id);
+return_doc(_, Error) ->
+    Error.
+
+%% @private
+maybe(ok, X) ->
+    {ok, X};
+maybe({ok, _}, X) ->
+    {ok, X};
+maybe(Else, _) ->
+    Else.
+
+%% @private
+strip_body(ok) ->
+    ok;
+strip_body({ok, _}) ->
+    ok;
+strip_body(Else) ->
+    Else.
+
+%% @private
+to_json(M) when is_map(M) ->
+    {maps:to_list(M)};
+to_json(L) when is_list(L) ->
+    {L};
+to_json({L} = X) when is_list(L) ->
+    X.
+
+%% @private
+get_value(K, {Doc}) ->
+    etbx:get_value(K, Doc);
+get_value(K, Doc) ->
+    etbx:get_value(K, Doc).
---
+++ src/erlaxed_client.erl
@@ -0,0 +1,118 @@
+%%
+%%  @author    Jan Krueger <jan.krueger@gmail.com>
+%%  @copyright 2015 Jan Krueger
+%%
+%%% This file is part of erlaxed released under the MIT license.
+%%
+
+-module(erlaxed_client).
+-compile({no_auto_import,[put/1, put/2]}).
+-export([delete/1, delete/2]).
+-export([get/1, get/2, get/3]).
+-export([put/1, put/2, put/3]).
+-export([post/2]).
+
+-define(DEFAULT_TIMEOUT, 5000).
+
+delete(Connection) ->
+    delete(Connection, <<>>).
+
+delete(Connection, Endpoint) ->
+    URL      = endpoint(Connection, Endpoint),
+    Response = httpc:request(delete, {URL, []}, [], []),
+    interpret_response(Response).
+
+get(Connection) ->
+    get(Connection, <<>>).
+
+get(Connection, Endpoint) ->
+    get(Connection, Endpoint, []).
+
+get(Connection, Endpoint, Options) ->
+    URL      = query(endpoint(Connection, Endpoint), Options),
+    Response = httpc:request(get, {URL, []}, [], []),
+    interpret_response(Response).
+
+put(Connection) ->
+    put(Connection, <<>>).
+
+put(Connection, Endpoint) ->
+    send(Connection, Endpoint, put, <<>>).
+
+put(Connection, Endpoint, Doc) ->
+    send(Connection, Endpoint, put, jiffy:encode(Doc)).
+
+post(Connection, Doc) ->
+    post(Connection, <<>>, Doc).
+
+post(Connection, Endpoint, Doc) ->
+    send(Connection, Endpoint, post, jiffy:encode(Doc)).
+
+%% @private
+send(Connection, Endpoint, Method, Body) ->
+    URL      = endpoint(Connection, Endpoint),
+    Response = httpc:request(
+                 Method, {URL, [], "application/json", Body},
+                 [], []),
+    interpret_response(Response).
+
+%% @private
+endpoint(Connection, Endpoint) ->
+    Server = etbx:get_value(host, Connection),
+    DB     = etbx:get_value(db,   Connection),
+    etbx:to_string(endpoint(Server, DB, Endpoint)).
+endpoint(Server, undefined, <<>>) ->
+    Server;
+endpoint(Server, undefined, Endpoint) ->
+    <<Server/binary, "/", Endpoint/binary>>;
+endpoint(Server, DB, <<>>) ->
+    <<Server/binary, "/", DB/binary>>;
+endpoint(Server, DB, Endpoint) ->
+    <<Server/binary, "/", DB/binary, "/", Endpoint/binary>>.
+
+%% @private
+query(URL, []) ->
+    URL;
+query(URL0, Options) ->
+    URL   = string:concat(URL0, "?"),
+    Query = lists:map(fun encode_option/1, Options),
+    string:concat(URL, string:join(Query, "&")).
+
+%% @private
+encode_option({key, Value}) ->
+    format_option("key", jiffy:encode(Value));
+encode_option({startkey, Value}) ->
+    format_option("startkey", jiffy:encode(Value));
+encode_option({endkey, Value}) ->
+    format_option("endkey", jiffy:encode(Value));
+encode_option({keys, Value}) when is_list(Value) ->
+    format_option("keys", jiffy:encode(Value));
+encode_option({Option, Value}) ->
+    format_option(Option, Value);
+encode_option(Option) ->
+    etbx:to_string(Option).
+
+format_option(Option, Value) ->
+    Query = etbx:to_string(Option),
+    Param = edoc_lib:escape_uri(etbx:to_string(Value)),
+    Query ++ "=" ++ Param.
+
+%% @private
+interpret_response({ok, {{_Version, Status, _Line}, _Headers, Body}}) ->
+    interpret_response({ok, {Status, Body}});
+interpret_response({ok, {200, Body}}) ->
+    {ok, jiffy:decode(Body)};
+interpret_response({ok, {201, Body}}) ->
+    {ok, jiffy:decode(Body)};
+interpret_response({ok, {204, _}}) ->
+    ok;
+interpret_response({ok, {400, _}}) ->
+    {error, bad_request};
+interpret_response({ok, {404, _}}) ->
+    {error, not_found};
+interpret_response({ok, {409, _}}) ->
+    {error, conflict};
+interpret_response({ok, {500, _}}) ->
+    {error, internal_error};
+interpret_response({error, Reason}) ->
+    {error, Reason}.
